8f4fe79db9da1289bc7f7288d9a657d6f0c47f55
Hilary Palevsky <hpalevsky@whoi.edu>
2018-08-13 10:23:56 -0400

    Update to add discrete samples from Irminger-3 cruise
    Now also reads in potential temperature and salinity, extracted for each Niskin in Yrs 2 and 3

--- loadWinklerIrminger.m
+++ loadWinklerIrminger.m
@@ -46,8 +46,10 @@ Yr2_disc.lon360 = -1*num(:,5) + 360; %degrees W - make negative to match convent
 Yr2_disc.depth = num(:,8); 
 Yr2_disc.oxy = num(:,10)/(0.0223916); %convert from mL/L to mmol/L; O2 = 22391.6 ml/mol (Garcia and Gordon, 1992)
 Yr2_disc.nitrate = num(:,12); %uM
+Yr2_disc.potT = num(:,13); %potential temp from CTD
+Yr2_disc.S = num(:,14); %salinity from CTD
 
-%Calculate uncertainty in Year 1 samples (take stdev of all 6 duplicates,
+%Calculate uncertainty in Year 2 samples (take stdev of all duplicates,
 %and use mean stdev value)
 for i = 1:6
     dups = Yr2_disc.oxy(2*i-1:2*i);
@@ -58,3 +60,32 @@ for i = 18:22
     duperr(i-11) = std(dups)
 end
 Yr2_disc.oxy_err = mean(duperr);
+
+%Year 3
+[num,txt,~]=xlsread('IrmingerYr3_AR07_CTDWaterSamplingData.xlsx');
+date = txt(2:end,3); %pull out text dates
+Yr3_disc.day = NaN*ones(length(txt)-1,1);
+for i = 1:length(num)
+    if cellfun('isempty',date(i)) == 1
+        Yr3_disc.day(i) = NaN;
+    else Yr3_disc.day(i) = datenum(date(i));
+    end
+end
+
+Yr3_disc.cast = num(:,1);
+Yr3_disc.time = num(:,3); %note that this is currently not formatted properly
+Yr3_disc.lat = num(:,4);
+Yr3_disc.lon360 = -1*num(:,5) + 360; %degrees W - make negative to match convention
+Yr3_disc.depth = num(:,8); 
+Yr3_disc.oxy = num(:,10)/(0.0223916); %convert from mL/L to mmol/L; O2 = 22391.6 ml/mol (Garcia and Gordon, 1992)
+Yr3_disc.nitrate = num(:,14); %uM
+Yr3_disc.potT = num(:,11); %potential temp from CTD
+Yr3_disc.S = num(:,12); %salinity from CTD
+
+%Calculate uncertainty in Year 3 samples (take stdev of all duplicates,
+%and use mean stdev value)
+for i = 1:10
+    dups = Yr3_disc.oxy(2*i-1:2*i);
+    duperr(i) = std(dups);
+end
+Yr3_disc.oxy_err = mean(duperr);
